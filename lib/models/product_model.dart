5b10bf3f35ad9772c515cd995863a44132ce14fc
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-16 22:29:13 +0300

    product model and rating model done

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,1 +1,38 @@
-class ProductModel {}
+class ProductModel {
+  final String title;
+  final String description;
+  final String image;
+  final double price;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.title,
+    required this.description,
+    required this.image,
+    required this.price,
+    required this.rating,
+  });
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      title: json['title'],
+      description: json['description'],
+      image: json['image'],
+      price: json['price'],
+      rating: RatingModel.fromJson(json['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  final double rate;
+  final int count;
+
+  RatingModel(this.rate, this.count);
+
+  factory RatingModel.fromJson(Map<String, dynamic> json) {
+    return RatingModel(
+      json['rate'],
+      json['count'],
+    );
+  }
+}
