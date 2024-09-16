a095364565aa1b5615541f813031ae1989a1f2e9
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-16 23:06:38 +0300

    import http package and complete GetProducts

--- lib/services/get_products.dart
+++ lib/services/get_products.dart
@@ -1,1 +1,21 @@
-class GetAllProduct {}
+import 'dart:convert';
+
+import 'package:products_app/models/product_model.dart';
+import 'package:http/http.dart' as http;
+
+class GetProducts {
+  Future<List<ProductModel>> getAllProducts() async {
+    Uri url = Uri.parse("https://fakestoreapi.com/products");
+    http.Response response = await http.get(url);
+
+    List<dynamic> data = jsonDecode(response.body);
+    List<ProductModel> products = [];
+    if (response.statusCode == 200) {
+      for (int i = 0; i < data.length; i++) {
+        products.add(ProductModel.fromJson(data[i]));
+      }
+    }
+
+    return products;
+  }
+}
