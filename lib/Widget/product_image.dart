b43e39c737680d959e521506459404e64c53f4ac
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-16 20:22:13 +0300

    refactor the code

---
+++ lib/Widget/product_image.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ProductImage extends StatelessWidget {
+  const ProductImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        alignment: Alignment.topRight,
+        decoration: BoxDecoration(
+          color: Colors.red,
+          borderRadius: BorderRadius.circular(5),
+          image: const DecorationImage(
+              image: NetworkImage(
+                "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+              ),
+              fit: BoxFit.fill),
+        ),
+        child: Container(
+          margin: const EdgeInsets.all(8),
+          height: 35,
+          width: 35,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20), color: Colors.white),
+          child: const Icon(
+            Icons.favorite_border,
+            size: 30,
+            color: Color.fromARGB(255, 0, 64, 116),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Widget/singleproduct_widget.dart
+++ lib/Widget/singleproduct_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:products_app/Widget/prdouct_details.dart';
+import 'package:products_app/Widget/product_image.dart';
 
 class SingleProductWidget extends StatelessWidget {
   const SingleProductWidget({super.key});
@@ -14,105 +16,13 @@ class SingleProductWidget extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
         border: Border.all(width: 2, color: const Color(0xffafc6dc)),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      child: const Column(
         children: [
-          Expanded(
-            child: Container(
-              width: double.infinity,
-              alignment: Alignment.topRight,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(5),
-                image: const DecorationImage(
-                    image: NetworkImage(
-                      "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-                    ),
-                    fit: BoxFit.fill),
-              ),
-              child: Container(
-                margin: const EdgeInsets.all(8),
-                height: 35,
-                width: 35,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white),
-                child: Icon(
-                  Icons.favorite_border,
-                  size: 30,
-                  color: const Color.fromARGB(255, 0, 64, 116),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
+          ProductImage(),
+          SizedBox(
             height: 5,
           ),
-          const Expanded(
-              child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Product Name",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff030051)),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Text(
-                  "Discrption Product",
-                  style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xff030051)),
-                  overflow: TextOverflow.ellipsis,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      "EGP 1200",
-                      style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xff030051)),
-                    ),
-                    SizedBox(
-                      width: 25,
-                    ),
-                    Text(
-                      "2000 EGP",
-                      style: TextStyle(
-                          color: Color(0xff239def),
-                          decoration: TextDecoration.lineThrough),
-                    )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("Review (4.6)",
-                        style: TextStyle(
-                            color: Color(0xff030051),
-                            fontWeight: FontWeight.w600)),
-                    Icon(
-                      Icons.star,
-                      color: Colors.yellow,
-                      size: 20,
-                    ),
-                    Spacer(),
-                    Icon(
-                      Icons.add_circle,
-                      color: Color(0xff004087),
-                      size: 40,
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ))
+          ProductDetails()
         ],
       ),
     );
