5d04e6ede9206e6406398363ee389b4523b686de
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-09-17 11:56:51 +0300

    provide Cubit to the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:products_app/Cubit/product_cubit/product_cubit.dart';
 import 'package:products_app/Views/product_home_page.dart';
 
 void main() {
@@ -13,7 +15,10 @@ class ProductsApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ProductHomePage(),
+      home: BlocProvider(
+        create: (context) => ProductCubit(),
+        child: const ProductHomePage(),
+      ),
       title: 'Products App',
       theme: ThemeData(
         fontFamily: GoogleFonts.poppins().fontFamily,
